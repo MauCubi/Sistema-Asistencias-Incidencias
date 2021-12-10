f9c9573b7246645a6744d19ce7f7ef1071617b67
Cristian Nicolas <nicolaspluhator@gmail.com>
2021-12-10 19:57:07 -0300

    Listado de asistencias de empleado logueado

---
+++ resources/views/asistencia/index.blade.php
@@ -0,0 +1,55 @@
+@extends('partials.admin')
+
+@section('content')
+
+<div class="row d-flex flex-row">
+    <div class="p-2">
+        <h3 class="text-primary">Mis asistencias</h3>
+    </div>
+</div>
+
+@include('partials.session-status')
+<p class="alert alert-info d-none d-sm-block animated fadeIn" >
+    Desde aquí, podrá <strong>ver todas sus asistencias registradas</strong>.
+</p>
+
+<table class="table table-bordered table-hover table-responsive-sm shadow table-sm">
+    <thead class="bg-dark text-white">
+        <tr>
+            <td>
+                Fecha
+            </td>
+            <td>
+                Ingreso
+            </td>
+            <td>
+                Salida
+            </td>            
+            <td>
+                
+            </td>
+        </tr>
+    </thead>
+    <tbody>
+       @foreach ($asistencias as $a)
+       <tr>
+            <td>
+                {{ date_format($a->start, " d/m/Y") }}
+            </td>
+            <td>
+                {{ date_format($a->start, " H:i:s") }}
+            </td>
+            <td>
+                {{ date_format($a->end, " H:i:s") }}
+            </td>
+            <td>
+                <div class="d-flex justify-content-center">
+                <a href="{{ route('asistencia.show', $a->id)}}" class="btn btn-primary mr-2 btn-sm"><i class="fa fw fa-info"></i> Ver</a>
+                </div>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@endsection
---
+++ resources/views/asistencia/show.blade.php
@@ -0,0 +1,50 @@
+@extends('partials.admin')
+
+
+@section('content')
+
+
+<div class="row d-flex flex-row">
+    <div class="p-2">
+        <h3 class="text-primary">{{ $asistencia->title }}
+        </h3>
+
+    </div>
+
+    <div class="ml-auto p-2 d-flex align-items-center">
+        <a href="{{ route('asistencia.index') }}">
+            <button class="btn btn-info btn-circle"><i class="fa fw fa-arrow-left"></i></button>
+        </a>
+    </div>
+
+</div> <!-- fin de row -->
+
+<div class="row py-2">
+    <div class="col-md-12">
+        <div class="card shadow border-bottom-info">
+            <div class="card-body">
+                <label class="control-label">
+                    <strong>Empleado:</strong>
+                    {{ $asistencia->empleado()->first()->apellido }}, {{ $asistencia->empleado()->first()->nombre }}.
+                </label>
+                <br>
+                <label>
+                	<strong>Fecha:</strong>
+                	{{ date_format($asistencia->start, " d/m/Y") }}
+                </label>
+                <br>
+                <label>
+                	<strong>Hora de ingreso: </strong>
+                	 {{ date_format($asistencia->start, " H:i:s") }}
+                </label>
+                <br>
+                <label>
+                	<strong>Hora de egreso: </strong>
+                	 {{ date_format($asistencia->end, " H:i:s") }}
+                </label>
+            </div>
+        </div>
+    </div>
+</div> <!-- fin de row -->
+
+@endsection
